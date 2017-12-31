7f3499b42f28d7c6867cd0fbf3c101cf515bb137
James Gary <bucket@earthlink.net>
2017-12-31 19:51:34 +0000

    automatically backed up by learn

--- listening.js
+++ listening.js
@@ -1,7 +1,11 @@
 // JavaScript for "Listening To Nodes"
 
-const main = document.getElementById('main')
+// const main = document.getElementById('main')
+//
+// main.addEventListener('click', function(event) {
+//   alert('I was clicked!')
+// })
 
-main.addEventListener('click', function(event) {
-  alert('I was clicked!')
+document.addEventListener('keydown', function(e) {
+  console.log(e.which)
 })
